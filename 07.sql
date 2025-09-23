fba05d5006e7849b003be2e71fa5d39e6149e425
wusd <nanxihope@163.com>
2025-09-23 11:36:58 +0800

    高性能MySQL- a new bootstrap

--- 07.sql
+++ 07.sql
@@ -68,20 +68,29 @@ limit 5;
 ### 加索引了
 alter table city_demo
     add key (city(7));
-explain select film_id,
-       actor_id from film_actor
-        where actor_id = 1 or film_id = 1;
+explain
+select film_id,
+       actor_id
+from film_actor
+where actor_id = 1
+   or film_id = 1;
 ## Choosing a Good Column Order
-select * from payment
-where staff_id = 2 and customer_id = 584;
-select sum(staff_id = 2), sum(customer_id = 584) from payment;
-select sum(staff_id = 2) from payment where customer_id = 584;
+select *
+from payment
+where staff_id = 2
+  and customer_id = 584;
+select sum(staff_id = 2), sum(customer_id = 584)
+from payment;
+select sum(staff_id = 2)
+from payment
+where customer_id = 584;
 ## 针对全体,比只针对特殊情况要好
-select count(distinct staff_id) / count(*) as staff_id_selectivity,
-       count(distinct customer_id)/count(*) as customer_id_selectivity,
+select count(distinct staff_id) / count(*)    as staff_id_selectivity,
+       count(distinct customer_id) / count(*) as customer_id_selectivity,
        count(*)
 from payment;
-alter table payment add key (customer_id, staff_id);
+alter table payment
+    add key (customer_id, staff_id);
 ### query running very slowly
 # select count(distinct threadId) as count_value from message
 # where (groupId = 10137) and (userId = 1288826) and (anonymous = 0)
@@ -107,3 +116,47 @@ alter table payment add key (customer_id, staff_id);
 # sum(anonymous = 0): 4141934
 -- 原来因为这个userId是admin, 这些数据都是迁移过来的.
 -- 解决: 程序对这个用户进行特殊处理.
+
+#### InnoDB's data layout
+create table layout_test
+(
+    col1 int not null,
+    col2 int not null,
+    primary key (col1),
+    key (col2)
+);
+
+####
+CREATE TABLE userinfo
+(
+    id              int unsigned      NOT NULL AUTO_INCREMENT,
+    name            varchar(64)       NOT NULL DEFAULT '',
+    email           varchar(64)       NOT NULL DEFAULT '',
+    password        varchar(64)       NOT NULL DEFAULT '',
+    dob             date                       DEFAULT NULL,
+    address         varchar(255)      NOT NULL DEFAULT '',
+    city            varchar(64)       NOT NULL DEFAULT '',
+    state_id        tinyint unsigned  NOT NULL DEFAULT '0',
+    zip             varchar(8)        NOT NULL DEFAULT '',
+    country_id      smallint unsigned NOT NULL DEFAULT '0',
+    gender          enum ('M', 'F')   NOT NULL DEFAULT 'M',
+    account_type    varchar(32)       NOT NULL DEFAULT '',
+    verified        tinyint           NOT NULL DEFAULT '0',
+    allow_mail      tinyint unsigned  NOT NULL DEFAULT '0',
+    parrent_account int unsigned      NOT NULL DEFAULT '0',
+    closest_airport varchar(3)        NOT NULL DEFAULT '',
+    PRIMARY KEY (id),
+    UNIQUE KEY email (email),
+    KEY country_id (country_id),
+    KEY state_id (state_id),
+    KEY state_id_2 (state_id, city, address)
+) ENGINE = InnoDB;
+
+### 只查索引不查行
+explain
+select store_id, film_id
+from inventory;
+### 只查索引不查行,有主键
+explain select actor_id, last_name
+from actor
+where last_name = 'HOPPER';
