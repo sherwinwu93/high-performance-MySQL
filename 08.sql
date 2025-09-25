5c090913ad4e46c205c5ff52833469593a15f229
wusd <nanxihope@163.com>
2025-09-25 15:21:20 +0800

    08

--- 08.sql
+++ 08.sql
@@ -80,7 +80,8 @@ from film
 where film.film_id > 500
   and film_actor.film_id > 500;
 # 所有join顺序都等价
-explain select film.film_id,
+explain
+select film.film_id,
        film.title,
        film.release_year,
        actor.actor_id,
@@ -89,13 +90,55 @@ explain select film.film_id,
 from film
          inner join film_actor using (film_id)
          inner join actor using (actor_id);
-explain select straight_join film.film_id,
-               film.title,
-               film.release_year,
-               actor.actor_id,
-               actor.first_name,
-               actor.last_name
-        from film
-                 inner join film_actor using (film_id)
-                 inner join actor using (actor_id);
+explain
+select straight_join film.film_id,
+                     film.title,
+                     film.release_year,
+                     actor.actor_id,
+                     actor.first_name,
+                     actor.last_name
+from film
+         inner join film_actor using (film_id)
+         inner join actor using (actor_id);
+# union的外部限制条件,不会优化到内部
+# 会先找出所有再取20条
+    (select first_name, last_name from actor order by last_name)
+    union all
+    (select first_name, last_name from customer order by last_name)
+    limit 20;
+## 优化下
+    (select first_name, last_name from actor order by last_name limit 20)
+    union all
+    (select first_name, last_name from customer order by last_name limit 20)
+    limit 20;
+# 一张表不可以既查询又更新
+# update tbl as outer_tbl
+# set c = (select count(*) from tbl as inner_tbl where inner_tabl.type = outer_tbl.type);
+# update tbl inner join(select type, count(*) as c from tbl group by type) as der using(type)
+# set tal.c = der.c;
+explain
+select count(*)
+from customer;
 
+## 优化offset
+select film_id, description
+from film
+order by title
+limit 50, 5;
+### 优化1,用covering index,而不是全部列
+select film_id, description
+from film
+         inner join (select film_id
+                     from film
+                     order by title
+                     limit 50, 5) as lim using (film_id);
+### 优化2,先计算position,索引position字段,再根据position查询
+# select film_id, description from film
+# where position between 50 and 54;
+# 记录最小的rental_id
+select * from rental
+order by rental_id desc limit 20;
+# 对rental_id进行限制
+select * from rental
+         where rental_id < 16030
+order by rental_id desc limit 20;
